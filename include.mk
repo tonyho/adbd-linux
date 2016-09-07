76d6530cc1a7cc0384e5a0e4e7e6dd21c54b64f6
Owen W. Taylor <otaylor@fishsoup.net>
2016-09-07 14:26:22 -0400

    Add a configure script and support builddir != srcddir
    Improve the non-Android Makefiles for adb to support a config.mk
    generated by the generated configure script, and also to support
    a builddir != srcdir setup if configure is run from a different
    directory.
    
    This allows the project to be built by a generic build system, see:
    https://people.gnome.org/~walters/build-api/build-api.md

---
+++ include.mk
@@ -0,0 +1,12 @@
+top_builddir ?= $(top_srcdir)
+-include $(top_builddir)/config.mk
+
+prefix ?= /usr
+exec_prefix ?= $(prefix)
+bindir ?= $(prefix)/bin
+sbindir ?= $(exec_prefix)/sbin
+datadir ?= $(prefix)/share
+mandir ?= $(datadir)/man
+sysconfdir ?= $(prefix)/etc
+OPT_CFLAGS ?= -O2 -g
+OPT_CXXFLAGS ?= -O2 -g
